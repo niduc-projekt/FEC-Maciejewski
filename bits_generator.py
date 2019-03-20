722f52d0b8d21468daf480dc693d143c29296cc5
Filip Gajewski <filipgajewski4@gmail.com>
2019-03-20 14:02:15 +0100

    First argument is a file name where to write bits

--- bits_generator.py
+++ bits_generator.py
@@ -1,7 +1,7 @@
 import random, sys
 
-with open('bits.txt','w') as file:
+with open(sys.argv[1],'w') as file:
     #print(sys.argv[1])
-    amount = int(sys.argv[1])
+    amount = int(sys.argv[2])
     for i in range(0,amount):
         file.write(str(random.randint(0,1)))

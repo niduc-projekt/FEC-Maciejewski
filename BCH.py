a9eb1abcaeeb1749e970cc2853644a4d704ed4e6
Filip Gajewski <40489424+werd0n4@users.noreply.github.com>
2019-05-05 14:35:26 +0200

    Update BCH.py

--- BCH.py
+++ BCH.py
@@ -1,9 +1,9 @@
 import bchlib
-import hashlib
 import os
 import random
 import numpy
-from base64 import b64encode
+
+from fromBitsStringToBytes import convBitsToBytes
 
 
 class BCH:
@@ -17,7 +17,7 @@ class BCH:
         ecc = self.obj.encode(data)
         packet = data + ecc
         return numpy.array(packet)
-        #return packet
+        # return packet
 
     def decode(self, packet):
         packet = bytearray(packet)
@@ -27,7 +27,7 @@ class BCH:
         data_decoded = decoded[1]
 
         return numpy.array(data_decoded)
-        #return data+ecc
+        # return data+ecc
 
     def bitflip(self, packet):
         byte_num = random.randint(0, len(packet) - 1)
@@ -36,26 +36,37 @@ class BCH:
         return packet
 
 
-my_BCH = BCH(8219,16)#tworzenie obiektu BCH
-data = os.urandom(512)#losowanie 512 bajtow danych
-print(numpy.array(bytearray(data)))#pokaz wylosowane bajty
+BCH_POLYNOMIAL = 8219
+BCH_BITS = 16
+
+my_BCH = BCH(BCH_POLYNOMIAL, BCH_BITS)  # tworzenie obiektu BCH
+# data = bytearray(os.urandom(512))#losowanie 512 bajtow danych
+data = convBitsToBytes('input.txt')
+print(numpy.array(bytearray(data)))  # pokaz wylosowane bajty
 print('\n')
 
-data_encoded = my_BCH.encode(data)#kodowanie danych
-print(data_encoded)#drukowanie zakodowanych danych
-print('\n')
+data_encoded = my_BCH.encode(data)  # kodowanie danych
+# print(data_encoded)#drukowanie zakodowanych danych
+# print('\n')
 
-for _ in range(1024):#psucie zakodowanych danych
+for _ in range(BCH_BITS):  # psucie zakodowanych danych
     data_coruptet = my_BCH.bitflip(data_encoded)
 
-print(data_coruptet)
+print(data_coruptet)  # pokaz popsute dane
 print('\n')
 
-data_decoded = my_BCH.decode((data_coruptet))#dekodowanie zepsutych danych
+data_decoded = my_BCH.decode((data_coruptet))  # dekodowanie zepsutych danych
 
+# print(data_decoded)#pokaz zakodowane dane
+
+blad = False
+for i in range(len(numpy.array(bytearray(data)))):
+    if numpy.array(bytearray(data))[i] != data_decoded[i]:
+        print('Blad!!!')
+        blad = True
+        break
+if (not blad):
+    print('Wszystko poprawnie!')
+
+# print(numpy.array(bytearray(data)))
 print(data_decoded)
-
-if numpy.array(bytearray(data)).all() == data_decoded.all():
-    print('udalo sie')
-else:
-    print('porazka')
